89d99bb3d0ba4056db9e300308877a77167c587d
AntonyLajes <tonymateuslajes@hotmail.com>
2025-03-04 10:10:45 -0300

    feat(error): added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import fastify from "fastify"
 import { petsRoutes } from "./http/controllers/pets/routes"
+import { ZodError } from "zod"
+import { env } from "./src"
 
 export const app = fastify()
 
 app.register(petsRoutes, {
     prefix: '/pets'
+})
+
+app.setErrorHandler((error, _, reply) => {
+    if(error instanceof ZodError) {
+        reply.status(400).send({
+            message: 'Invalid Params',
+            error: error.format()
+        })
+    }
+
+    if(env.NODE_ENV === "dev"){
+        console.log(error)
+    }
+
+    reply.status(500).send({
+        message: 'Internal server error.'
+    })
 })
