b59b63e528b0b6978ed952c80454209240ec6185
AntonyLajes <tonymateuslajes@hotmail.com>
2025-03-05 10:17:51 -0300

    test(setup): setup vitest supertest e2e & unit workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,27 @@ import tsConfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
     plugins: [
         tsConfigPaths()
-    ]
+    ],
+    test: {
+        workspace: [
+            {
+                extends: true,
+                test: {
+                    environment: './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+                    include: [
+                        'src/http/controllers/**/*.spec.ts'
+                    ],
+                    name: 'e2e'
+                }
+            },
+            {
+                extends: true,
+                test: {
+                    environment: 'node',
+                    include: ['src/use-cases/**/*.spec.ts'],
+                    name: 'unit'
+                }
+            }
+        ]
+    }
 })
